9e0abeed54221877b5f9ef6bdff3958e9cb856d4
coustem-Coder <gohelnikunj451@gmail.com>
2024-07-12 08:58:22 +0530

    for some add feature

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ loginbtn.addEventListener("click",()=>
 {
 	window.open("/login.html");
 })
+
+let emailBtn=document.getElementById("emailBtn");
+
+
+emailBtn.addEventListener("click",()=>{
+	
+	let email=document.getElementById("email").value;
+	console.log(email)
+
+	if(email!="gohelnikunj451@gmail.com") {
+		window.open("404.html");
+	}
+	else{
+		window.open("movielist.html");
+	}
+})
